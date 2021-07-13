e0e9ae8be59c44e7065b8972c499ea1fca0b43bb
Mario Pardo <mariio.pardojr@gmail.com>
2021-07-12 21:06:59 -0300

    Add elemento com texto em desenvolvimento

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import StrangerThings from './components/StrangerThings';
 function App() {
   return (
     <div className="App">
+      <header>Em desenvolvimento</header>
       <StrangerThings />
     </div>
   );
