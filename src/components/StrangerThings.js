cff577e50fb04c09695f87c5317edf2200a12d17
Mario Pardo <mariio.pardojr@gmail.com>
2021-07-12 21:55:59 -0300

    Add tag em desenvolvimento

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -113,7 +113,7 @@ class StrangerThings extends React.Component {
           hereIsTheUpsideDownWorld,
         )}` }
       >
-        <p className="dev">Em desenvolvimento</p>
+        {process.env.IS_IT_DEV === 'true' && <p className="dev">Em desenvolvimento</p>}
         <div className="content strangerfy">
           <div className="change-reality">
             <button type="button" onClick={ this.changeRealityClick }>
