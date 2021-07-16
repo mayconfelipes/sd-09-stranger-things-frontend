67aecc3649e2428a6b50bd374a1359ae162ba2dc
Lucas Neiva <lucassneiva.code@gmail.com>
2021-07-16 18:57:33 -0300

    add development enviroment

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import StrangerThings from './components/StrangerThings';
 function App() {
   return (
     <div className="App">
+      <span>Em desenvolvimento</span>
       <StrangerThings />
     </div>
   );
