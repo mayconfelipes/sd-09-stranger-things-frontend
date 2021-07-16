95ee44d10a7d3466c99a70bdb6ba034132c9c3e8
Lucas Neiva <lucassneiva.code@gmail.com>
2021-07-16 19:03:01 -0300

    development tag off

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import StrangerThings from './components/StrangerThings';
 function App() {
   return (
     <div className="App">
-      <span>Em desenvolvimento</span>
       <StrangerThings />
     </div>
   );
