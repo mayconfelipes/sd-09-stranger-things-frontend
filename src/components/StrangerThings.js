158a64c3b91c4cd6e86d3a336fa43adb3179517f
johnealves <johne_af@hotmail.com>
2021-07-22 19:55:06 -0300

    add tag "em desenvolvimento"

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -113,6 +113,7 @@ class StrangerThings extends React.Component {
           hereIsTheUpsideDownWorld,
         )}` }
       >
+        <h1>Em desenvolvimento</h1>
         <div className="content strangerfy">
           <div className="change-reality">
             <button type="button" onClick={ this.changeRealityClick }>
