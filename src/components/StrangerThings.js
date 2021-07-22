74cbe882a5ec1e236f702ca18deb333ffed167d5
Khyrki <khyrkimitsunobi@gmail.com>
2021-07-22 17:21:31 -0300

    remove tag

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -114,7 +114,6 @@ class StrangerThings extends React.Component {
         )}` }
       >
         <div className="content strangerfy">
-          <h1>Em desenvolvimento</h1>
           <div className="change-reality">
             <button type="button" onClick={ this.changeRealityClick }>
               {' '}
