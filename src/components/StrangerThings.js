f013d428066ae4c9e4db8294754d841c61462cae
BernardoFontesNKWEB <bernardo.miranda@nkweb.com.br>
2021-07-22 19:10:28 -0300

    a

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -6,6 +6,8 @@ const getRealityClass = (hereIsTheUpsideDownWorld) => (
   hereIsTheUpsideDownWorld ? 'upside-down' : 'stranger-things'
 );
 
+// aaaa
+
 const strangerThingsConfig = {
   url: process.env.REACT_APP_HAWKINS_URL,
   timeout: process.env.REACT_APP_HAWKINS_TIMEOUT,
