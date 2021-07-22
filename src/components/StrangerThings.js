c33283d8df56863b146b356813e4e8678a1a835e
johnealves <johne_af@hotmail.com>
2021-07-22 19:12:18 -0300

    add file .env

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -7,12 +7,12 @@ const getRealityClass = (hereIsTheUpsideDownWorld) => (
 );
 
 const strangerThingsConfig = {
-  url: 'http://localhost:3002',
+  url: 'http://johnealves-bk.herokuapp.com/',
   timeout: 30000,
 };
 
 const upsideDownConfig = {
-  url: 'http://localhost:3003',
+  url: 'http://johnealves-bd.herokuapp.com/',
   timeout: 30000,
 };
 
