d5f4dc6ac72ffcbfd5bec9d7d9d641449788014b
tiago-yoneda <xyonedax@gmail.com>
2021-07-22 17:19:07 -0300

    fazendo o site development

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -147,6 +147,7 @@ class StrangerThings extends React.Component {
             <button type="button" onClick={ this.nextPage }>Próximo</button>
           </div>
         </div>
+        <p>Em desenvolvimento</p>
       </div>
     );
   }
