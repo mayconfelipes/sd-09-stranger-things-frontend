7476d23b2f5184f75f4a8775fd781acd24a90630
jhon <jhonatan.arguello@gmail.com>
2021-07-22 19:19:33 -0300

    Finalizaçao do projeto.

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -10,12 +10,12 @@ const getRealityClass = (hereIsTheUpsideDownWorld) => (
 
 const strangerThingsConfig = {
   url: process.env.REACT_APP_HAWKINS_URL,
-  timeout: 30000,
+  timeout: process.env.REACT_APP_HAWKINS_TIMEOUT,
 };
 
 const upsideDownConfig = {
   url: process.env.REACT_APP_UPSIDEDOWN_URL,
-  timeout: 30000,
+  timeout: process.env.REACT_APP_UPSIDEDOWN_TIMEOUT,
 };
 
 const charactersService = new CharactersService(strangerThingsConfig);
