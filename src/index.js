32fde5d5c0c3c624da17acb5db2eec1e94f8ed1f
julianafatsil <julianafatsil@gmail.com>
2021-07-22 16:10:52 -0300

    first commit stranger-things-frontend

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+// coments
 ReactDOM.render(
   <React.StrictMode>
     <App />
