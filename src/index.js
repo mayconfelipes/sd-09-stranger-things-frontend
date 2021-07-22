8efa3c80cd842923cae2cde741d32cf0930fe93f
tiago-yoneda <xyonedax@gmail.com>
2021-07-22 12:52:13 -0300

    Tiago Yoneda - Project Stranger Things Frontend - Initial Commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+// Tiago Yoneda
 ReactDOM.render(
   <React.StrictMode>
     <App />
