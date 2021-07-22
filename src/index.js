740eddd9f1f6da31cd1b0aec0b8ef6b78098a663
tiago-yoneda <xyonedax@gmail.com>
2021-07-22 14:58:03 -0300

    tentando dar um fix no github v2

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-// Tiago Yoneda - fix
+// Tiago Yoneda - fix2
 ReactDOM.render(
   <React.StrictMode>
     <App />
