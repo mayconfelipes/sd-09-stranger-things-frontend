6c6a949f264d99f8d57f160694bffc649806c1c1
Henrique Brito Elias <henriquebelias@gmail.com>
2021-07-22 16:39:07 -0300

    requisito bonus - producao

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -152,7 +152,6 @@ class StrangerThings extends React.Component {
             <button type="button" onClick={ this.nextPage }>Próximo</button>
           </div>
         </div>
-        <p>Em desenvolvimento</p>
       </div>
     );
   }
