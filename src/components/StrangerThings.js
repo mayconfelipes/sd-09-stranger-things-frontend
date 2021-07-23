f5e6d03ad035f8cec7560e4d0badf0cee003fbb7
Rafael Pufal <rafael.pufal23@gmail.com>
2021-07-23 13:54:54 -0300

    desenvolvimento

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -148,6 +148,7 @@ class StrangerThings extends React.Component {
             <button type="button" onClick={ this.previousPage }>Anterior</button>
             <button type="button" onClick={ this.nextPage }>Próximo</button>
           </div>
+          <h2>Em desenvolvimento</h2>
         </div>
       </div>
     );
