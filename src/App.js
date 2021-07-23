67cf7645917301d82d4fc0aaed7a087a925e5e6f
Lucas Silva <martssilvapro@gmail.com>
2021-07-23 16:30:49 -0300

    adiciona tag em desenvolvimento

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import StrangerThings from './components/StrangerThings';
 function App() {
   return (
     <div className="App">
+      <p>Em desenvolvimento</p>
       <StrangerThings />
     </div>
   );
