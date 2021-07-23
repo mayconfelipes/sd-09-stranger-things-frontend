59bd0299cc7455cf4a943b6b8a3e23d77b2f3c32
Carine Becker <caarine.m@gmail.com>
2021-07-23 13:29:26 -0300

    config variaveis do ambiente

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -2,23 +2,20 @@ import React from 'react';
 import CharactersService from '../services/charactersAPI';
 import Table from './Table';
 
+require('dotenv').config();
+
 const getRealityClass = (hereIsTheUpsideDownWorld) => (
   hereIsTheUpsideDownWorld ? 'upside-down' : 'stranger-things'
 );
 
-/* PRESTENÇÃO
-  require('dotenv').config();
-  process.env.NOMEDOAMBIENTE
-*/
-
 const strangerThingsConfig = {
-  url: 'http://localhost:3002',
-  timeout: 30000,
+  url: process.env.REACT_APP_HAWKINS_URL,
+  timeout: process.env.REACT_APP_HAWKINS_TIMEOUT,
 };
 
 const upsideDownConfig = {
-  url: 'http://localhost:3003',
-  timeout: 30000,
+  url: process.env.REACT_APP_UPSIDEDOWN_URL,
+  timeout: process.env.REACT_APP_UPSIDEDOWN_TIMEOUT,
 };
 
 const charactersService = new CharactersService(strangerThingsConfig);
