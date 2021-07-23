68c086990aafcfa25846af50de546989820888bd
Lucas Silva <martssilvapro@gmail.com>
2021-07-23 16:56:47 -0300

    corrige ternario

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import StrangerThings from './components/StrangerThings';
 function App() {
   return (
     <div className="App">
-      { process.env.REACT_APP_ENVIROMENT ? 'development' : <p>Em desenvolvimento</p>}
+      { process.env.REACT_APP_ENVIROMENT ? <p>Em desenvolvimento</p> : null}
       <StrangerThings />
     </div>
   );
