e0e36550f456cd3d5471b49c9247f4ba7f88dd43
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-07-23 22:29:55 -0300

    changind worlds

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -117,6 +117,7 @@ class StrangerThings extends React.Component {
         )}` }
       >
         <div className="content strangerfy">
+          <div>Em Desenvolvimento</div>
           <div className="change-reality">
             <button type="button" onClick={ this.changeRealityClick }>
               {' '}
