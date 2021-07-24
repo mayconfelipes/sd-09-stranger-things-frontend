b301d5b83d3a06fae0de4dd9d6087e5f2183f26b
Rafael Reis <rafaelrnascimento2006@gmail.com>
2021-07-23 22:20:23 -0300

    development

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import StrangerThings from './components/StrangerThings';
 function App() {
   return (
     <div className="App">
+      { process.env.REACT_APP_DEVELOPMENT && <span>Em desenvolvimento</span> }
       <StrangerThings />
     </div>
   );
