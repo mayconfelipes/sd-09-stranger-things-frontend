08f81ba2bb07d3ffd269f07d7039dbded7539267
marcusWittho <witthoeft.mcs@hotmail.com>
2021-07-26 19:37:32 -0300

    Correção do timeout

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -10,12 +10,12 @@ const getRealityClass = (hereIsTheUpsideDownWorld) => (
 
 const strangerThingsConfig = {
   url: process.env.REACT_APP_HAWKINS_URL,
-  timeout: process.env.REACT_APP_HAWKINS_TIMEOUT,
+  timeout: Number(process.env.REACT_APP_HAWKINS_TIMEOUT),
 };
 
 const upsideDownConfig = {
   url: process.env.REACT_APP_UPSIDEDOWN_URL,
-  timeout: process.env.REACT_APP_UPSIDEDOWN_TIMEOUT,
+  timeout: Number(process.env.REACT_APP_UPSIDEDOWN_TIMEOUT),
 };
 
 const charactersService = new CharactersService(strangerThingsConfig);
