1b9f802b5c5a719bb78e5fd0be5a0610bb899fc1
Rmgreco <rafaelmgreco@hotmail.com>
2021-07-29 17:04:54 -0300

    front

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import './App.css';
+import dotenv from 'dotenv';
 
 import StrangerThings from './components/StrangerThings';
+
+dotenv.config();
 
 function App() {
   return (
