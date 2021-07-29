889952df54535ac0337c6a87a0303876865205d8
Pablo Fernandes <pabloprates@gmail.com>
2021-07-29 09:46:50 -0300

    Adding the environment variables

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import './App.css';
+import StrangerThings from './components/StrangerThings';
 
 require('dotenv/config');
 
-import './App.css';
 
-import StrangerThings from './components/StrangerThings';
 
 function App() {
   return (
