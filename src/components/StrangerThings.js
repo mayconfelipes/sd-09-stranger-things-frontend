2f3c0321b371100c4ed725f6d314a3c4905205f5
mayconfelipes <mayconfelipe.dev@gmail.com>
2021-07-29 10:07:38 -0300

    testing1

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -7,12 +7,12 @@ const getRealityClass = (hereIsTheUpsideDownWorld) => (
 );
 
 const strangerThingsConfig = {
-  url: 'http://localhost:3002',
+  url: 'http://localhost:3000',
   timeout: 30000,
 };
 
 const upsideDownConfig = {
-  url: 'http://localhost:3003',
+  url: 'http://fierce-reef-42306.herokuapp.com/',
   timeout: 30000,
 };
 
