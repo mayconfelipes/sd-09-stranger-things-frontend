15059f143442e285a8c640d77226d1c78a60e294
Renato Feitosa <pythaqua@gmail.com>
2021-07-30 02:05:14 -0300

    build(env): Change dev var

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import StrangerThings from './components/StrangerThings';
 function App() {
   return (
     <div className="App">
+      { process.env.REACT_APP_IS_DEV_MODE && <div>Em desenvolvimento</div> }
       <StrangerThings />
     </div>
   );
