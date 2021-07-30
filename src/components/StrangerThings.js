e46d7b81480b02c75ac8b3155e379df09fc45284
julianafatsil <julianafatsil@gmail.com>
2021-07-30 14:07:51 -0300

    Build tags

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import CharactersService from '../services/charactersAPI';
 import Table from './Table';
+
 require('dotenv/config');
 
-const { REACT_APP_HAWKINS_URL, REACT_APP_HAWKINS_TIMEOUT,REACT_APP_UPSIDEDOWN_URL,
-   REACT_APP_UPSIDEDOWN_TIMEOUT } = process.env;
+const { REACT_APP_HAWKINS_URL, REACT_APP_HAWKINS_TIMEOUT,REACT_APP_UPSIDEDOWN_URL, 
+  REACT_APP_UPSIDEDOWN_TIMEOUT } = process.env;
 const getRealityClass = (hereIsTheUpsideDownWorld) => (
   hereIsTheUpsideDownWorld ? 'upside-down' : 'stranger-things'
 );
