30a090b6d469a87af4d8659507e493e5a2078b6c
joao <joaocarlitoneto@gmail.com>
2021-08-01 20:22:56 -0300

    Fazendo deploy

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import React from 'react';
 import './App.css';
 
 import StrangerThings from './components/StrangerThings';
+import TagComponent from './components/TagComponent';
+
+require('dotenv').config();
+
+const TagDev = process.env.TAG_DEV === 'true';
 
 function App() {
   return (
     <div className="App">
+      { TagComponent && TagDev }
       <StrangerThings />
     </div>
   );
---
+++ src/components/TagComponent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Dev = () => (
+  <div>
+    <p>Em desenvolvimento</p>
+  </div>
+);
+
+export default Dev;
