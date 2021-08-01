a1bce3865728447c0b54be788a816cc6dcd97c28
patriciavsilva <silva.patricia.v@gmail.com>
2021-08-01 18:08:28 -0300

    req 9

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './App.css';
 
 import StrangerThings from './components/StrangerThings';
+import DevelopmentTag from './components/DevelopmentTag';
 
+const developmentMode = process.env.REACT_APP_DEVELOPMENT === 'true';
 function App() {
   return (
     <div className="App">
       <StrangerThings />
+      { developmentMode && <DevelopmentTag /> }
+
     </div>
   );
 }
---
+++ src/components/DevelopmentTag.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const DevelopmentTag = () => (
+  <div>
+    <p>Em desenvolvimento</p>
+  </div>
+);
+
+export default DevelopmentTag;
