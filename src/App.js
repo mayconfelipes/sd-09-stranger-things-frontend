2766989c6558ec1e0cdb82201b725b630324455e
RodrigoNeoEx <draja.acc@gmail.com>
2021-08-02 19:26:09 -0300

    lots of errors on npm

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
-require('dotenv').config();
 
 import StrangerThings from './components/StrangerThings';
 
 function App() {
   return (
     <div className="App">
-      { process.env.REACT_APP_DEVELOPMENT && <span>Em desenvolvimento</span> }
       <StrangerThings />
     </div>
   );
