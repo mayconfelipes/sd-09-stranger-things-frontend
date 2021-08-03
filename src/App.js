23bf1a06840c49185cee9bd09e8408307832f3ec
RodrigoNeoEx <draja.acc@gmail.com>
2021-08-03 09:05:52 -0300

    add development mode

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React from 'react';
 import StrangerThings from './components/StrangerThings';
 import './App.css';
 
+require('dotenv').config();
+
 function App() {
   return (
     <div className="App">
+      { process.env.DEVELOPMENT_MODE && <p>Em desenvolvimento</p> }
       <StrangerThings />
     </div>
   );
