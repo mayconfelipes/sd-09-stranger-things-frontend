aaf375b747c9c57534ed6af8f8e55e5a755b0aeb
Natanael Neto <natanael.eneas2018@hotmail.com>
2021-08-03 14:42:14 -0300

    Teste req 9 um dia sai

--- src/App.js
+++ src/App.js
@@ -3,19 +3,12 @@ import './App.css';
 
 import StrangerThings from './components/StrangerThings';
 
-const isDevMode = process.env.DEV_MODE === 'true';
-console.log(process.env.DEV_MODE);
 function App() {
-  return isDevMode
-    ? (
-      <div>
-        <p>Em desenvolvimento</p>
-      </div>)
-    : (
-      <div className="App">
-        <StrangerThings />
-      </div>
-    );
+  return (
+    <div className="App">
+      <StrangerThings />
+    </div>
+  );
 }
 
 export default App;
--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -7,7 +7,10 @@ const {
   REACT_APP_HAWKINS_TIMEOUT,
   REACT_APP_UPSIDEDOWN_URL,
   REACT_APP_UPSIDEDOWN_TIMEOUT,
+  DEV_MODE,
 } = process.env;
+
+console.log(DEV_MODE);
 
 const getRealityClass = (hereIsTheUpsideDownWorld) => (
   hereIsTheUpsideDownWorld ? 'upside-down' : 'stranger-things'
