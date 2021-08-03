b479297d6ecfe0557234a6e5b85efd489ae92547
Natanael Neto <natanael.eneas2018@hotmail.com>
2021-08-03 14:35:04 -0300

    Teste req 9 final v3

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import StrangerThings from './components/StrangerThings';
 
 const isDevMode = process.env.DEV_MODE === 'true';
-
+console.log(isDevMode);
 function App() {
   return isDevMode
     ? (
