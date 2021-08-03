d1825493a6fcc86864c28760d75392914b5cd03e
ViniciusChagas <viniciushrcs@gmail.com>
2021-08-03 12:48:43 -0300

    Alteração no componente StrangerThings

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -2,8 +2,6 @@ import React from 'react';
 import CharactersService from '../services/charactersAPI';
 import Table from './Table';
 
-require('dotenv').config();
-
 const getRealityClass = (hereIsTheUpsideDownWorld) => (
   hereIsTheUpsideDownWorld ? 'upside-down' : 'stranger-things'
 );
