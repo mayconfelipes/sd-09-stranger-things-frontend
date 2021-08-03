908d121a6bed282c770c291a585232a0f16bea2c
Natanael Neto <natanael.eneas2018@hotmail.com>
2021-08-03 16:03:26 -0300

    Teste vn

--- src/App.js
+++ src/App.js
@@ -3,20 +3,12 @@ import './App.css';
 
 import StrangerThings from './components/StrangerThings';
 
-const isDevMode = process.env.REACT_APP_DEV_MODE;
-console.log(isDevMode);
-
 function App() {
-  return isDevMode
-    ? (
-      <div>
-        <p>Em desenvolvimento</p>
-      </div>)
-    : (
-      <div className="App">
-        <StrangerThings />
-      </div>
-    );
+  return (
+    <div className="App">
+      <StrangerThings />
+    </div>
+  );
 }
 
 export default App;
