413cbb943a0d5db5095f31fbc3c824d28726e254
antonioarieiro <sfgfffs@gmail.com>
2021-08-05 19:13:01 -0300

    fix all

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 
 import StrangerThings from './components/StrangerThings';
 
+/* PROJETO FINALIZADO EM PARCERIA COM A TRICA */
+
 function App() {
   return (
     <div className="App">
