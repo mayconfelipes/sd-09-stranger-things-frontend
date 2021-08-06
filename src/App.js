9f44ef925136b432c684b4d0f2a14641f50ca079
Natanael Neto <natanael.eneas2018@hotmail.com>
2021-08-05 23:20:23 -0300

    Vai, em nome do Grande

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import './App.css';
 
 import StrangerThings from './components/StrangerThings';
 
+const { REACT_APP_DEV_MODE } = process.env;
 function App() {
-  return (
+  return REACT_APP_DEV_MODE ? (
+    <div className="App">
+      <p>Em Desenvolvimento</p>
+      <StrangerThings />
+    </div>
+  ) : (
     <div className="App">
       <StrangerThings />
     </div>
