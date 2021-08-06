9e05f71597871416dcf7d422171e4eb7c62592e0
Alana Monteiro <lanaah.15@gmail.com>
2021-08-06 00:01:04 -0300

    em desenvolvimento

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -113,6 +113,7 @@ class StrangerThings extends React.Component {
           hereIsTheUpsideDownWorld,
         )}` }
       >
+        <div>Em desenvolvimento</div>
         <div className="content strangerfy">
           <div className="change-reality">
             <button type="button" onClick={ this.changeRealityClick }>
